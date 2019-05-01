b5db1848516989c3e68f001c73cf55c2c98bd97d
Cristian Camarero <cristian.j.camarero@gmail.com>
2019-05-01 13:20:20 -0700

    completed test suite

--- test/app.test.js
+++ test/app.test.js
@@ -4,8 +4,100 @@ const request = require('supertest');
 
 describe('GET /apps endpoint', () => {
   it('should return an array of apps', () => {
-
+    return request(app)
+      .get('/apps')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then(res => {
+        expect(res.body).to.be.an('array');
+        expect(res.body).to.have.lengthOf.at.least(1);
+        const app = res.body[0]
+        expect(app).to.include.all.keys('App', 'Rating', 'Genres')
+      })
   });
 
-  
+  it('should filter apps by genres term: Puzzle', () => {
+    expected = [
+      {
+          "App": "Block Puzzle",
+          "Category": "GAME",
+          "Rating": 4.6,
+          "Reviews": "59800",
+          "Size": "7.8M",
+          "Installs": "5,000,000+",
+          "Type": "Free",
+          "Price": "0",
+          "Content Rating": "Everyone",
+          "Genres": "Puzzle",
+          "Last Updated": "March 6, 2018",
+          "Current Ver": "2.9",
+          "Android Ver": "2.3 and up"
+      },
+      {
+          "App": "Block Puzzle Classic Legend !",
+          "Category": "GAME",
+          "Rating": 4.2,
+          "Reviews": "17039",
+          "Size": "4.9M",
+          "Installs": "5,000,000+",
+          "Type": "Free",
+          "Price": "0",
+          "Content Rating": "Everyone",
+          "Genres": "Puzzle",
+          "Last Updated": "April 13, 2018",
+          "Current Ver": "2.9",
+          "Android Ver": "2.3.3 and up"
+      }
+    ]
+
+    return request(app)
+      .get('/apps')
+      .query({genres: 'Puzzle'})
+      .expect(200, expected)
+      .expect('Content-Type', /json/)
+  });
+
+  it('apps can only be sorted by Rating or App', () => {
+    return request(app)
+      .get('/apps')
+      .query({sort: 'wrong'})
+      .expect(400, 'Apps can only only be sorted by rating or app');
+  });
+
+  it('sorting by Rating', () => {
+    return request(app)
+      .get('/apps')
+      .query({sort: 'Rating'})
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then(res => {
+        expect(res.body).to.be.an('array');
+        let i = 0;
+        let sorted = true;
+        while(sorted && i < res.body.length - 1) {
+          sorted = sorted && res.body[i].Rating >= res.body[i + 1].Rating;
+          i++;
+        }
+        expect(sorted).to.be.true;
+      });
+  });
+
+  it('sorting by App', () => {
+    return request(app)
+      .get('/apps')
+      .query({sort: 'App'})
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then(res => {
+        expect(res.body).to.be.an('array');
+        let i = 0;
+        let sorted = true;
+        while(sorted && i < res.body.length - 1) {
+          sorted = sorted && res.body[i].App < res.body[i + 1].App;
+          i++;
+        }
+        expect(sorted).to.be.true;
+      });
+  });
+
 });
